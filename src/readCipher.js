66203b5ef9b23f59d31db062ae5872438a6c6602
AntMousinho <antony.mousinho@hotmail.co.uk>
2021-03-11 17:53:33 +0000

    Changing  split(\r\n) to split(os.EOL) for use on other OS

--- src/readCipher.js
+++ src/readCipher.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
+const os = require('os');
 
 const readCipher = (file) => {
     let data = fs.readFileSync(file, 'utf8');
     let result = {}
-    data.split('\r\n').map(pair => pair.split(', ')).forEach(splitPair => result[splitPair[0]] = splitPair[1])
+    data.split(os.EOL).map(pair => pair.split(', ')).forEach(splitPair => result[splitPair[0]] = splitPair[1])
     return  result;
 }
 
